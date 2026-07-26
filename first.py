3f66896c911f25cc828504832b1a658583d60ba3
Vedant Singh <inboxvedantsingh@gmail.com>
2026-07-27 03:23:05 +0530

    new file

---
+++ first.py
@@ -0,0 +1,11 @@
+# first pyton program
+# print("Hello World")
+
+#variables
+a = 6
+print(a)
+b = "6"
+print(b)
+#typecasting
+print(a+int(b))
+
---
+++ module.py
@@ -0,0 +1,4 @@
+import pyjokes
+# print("Hello, here is a joke for you:")
+joke = pyjokes.get_joke()
+print(joke)
