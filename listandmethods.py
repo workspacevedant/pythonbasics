175fe5d2b31c0f7d3c85e74cac43283ea84bbe39
Vedant Singh <inboxvedantsingh@gmail.com>
2026-07-28 23:50:59 +0530

    mutation of lists

--- listandmethods.py
+++ listandmethods.py
@@ -1,9 +1,10 @@
-l1 = [1, 2, 3, "vedant"]
+l1 = [1, 2, 3, 7, 0, 6, 76, 8, "vedant"]
 print(type(l1))
 print(l1)
 #string is immutable, but lists can be mutated
 
 l1.remove("vedant")
 
+l1.sort()
 print(l1)
 
