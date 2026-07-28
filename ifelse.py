2befe95a8089770a4bbca5e09471836204d1f860
Vedant Singh <inboxvedantsingh@gmail.com>
2026-07-29 03:45:24 +0530

    elif

--- ifelse.py
+++ ifelse.py
@@ -1,6 +1,10 @@
 age = int(input("Enter your age: "))
 
-if(age>18):
+if(age>=18):
     print("dhoom machale")
+elif(age<8):
+    print("kiddo")
 else:
     print("stay home be safe")
+print(age)
+print("end of program")
