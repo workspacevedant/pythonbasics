6810daaf46ef1560f9e7b3cbbfe950f12b034df1
Vedant Singh <inboxvedantsingh@gmail.com>
2026-07-28 15:10:01 +0530

    user input

---
+++ userinput.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+print("your name is : " + name)
+print(type(name))
+
+# take input from user and 6 to it and mutiply it by 2
+num1 = int(input("Enter your first number: "))
+print(type(num1))
+num1 = (num1 + 6) * 2
+print("your first number with 6 added and then mutiplied by 2 is : " + str(num1))
