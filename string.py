eb899094838de646aa0ffa1995894a35b1e7f822
Vedant Singh <inboxvedantsingh@gmail.com>
2026-07-28 15:00:54 +0530

    new string functions

--- string.py
+++ string.py
@@ -1,3 +1,12 @@
 name = "vedant"
+number = "7"
 print(name)
 print(name [2:4]) #trimming
+
+print(name.capitalize())
+print(name.title())
+print(name.lower())
+print(name.upper())
+print(name.count("a"))
+print(number.isdigit())
+print(number.isnumeric())
