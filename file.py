59111997f1d2701b750245f0c8159c47d2cf8185
Vedant Singh <inboxvedantsingh@gmail.com>
2026-07-29 19:42:43 +0530

    appending

--- file.py
+++ file.py
@@ -1,4 +1,21 @@
 s = "vedant is a good boi"
 
-with open("vedant.txt", "w") as f:
-    f.write(s)
+# reading a file
+
+with open("test.txt", "r") as f:
+    s = f.read()
+    print(s)
+
+
+
+
+#with open("vedant.txt", "w") as f:
+    #f.write(s)
+
+# fp = open("test.txt", "w")
+# fp.write(s)
+# fp.close()
+
+
+with open("test.txt", "a") as f:
+    f.write(" and is very nice")
